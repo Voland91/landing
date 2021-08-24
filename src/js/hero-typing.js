2086d44ee8750590b03e9a8be114d859fca5c1a7
Mateusz Butkiewicz <butkiewicz.mat@gmail.com>
2021-08-24 12:18:23 +0200

    added animation typing to hero text

---
+++ src/js/hero-typing.js
@@ -0,0 +1,20 @@
+const textPlace = document.querySelector('.hero-text');
+const cursor = document.querySelector('.hero-cursor');
+
+//this is text you want to type in hero h2
+const txt = "infinity-group";
+
+let indexText = 0;
+
+const addLetter = () => {
+    textPlace.textContent += txt[indexText];
+    indexText++;
+    if (indexText === txt.length) clearInterval(indexTyping);
+}
+
+const cursorAnimation = () => {
+    cursor.classList.toggle('active-cursor');
+}
+
+const indexTyping = setInterval(addLetter, 200);
+setInterval(cursorAnimation, 500);
