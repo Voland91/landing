07ebc885f072b4dfddf9b2414f13e02ae98b0229
Mateusz Butkiewicz <butkiewicz.mat@gmail.com>
2021-08-25 15:26:50 +0200

    added slider

--- src/js/slider.js
+++ src/js/slider.js
@@ -1,8 +1,25 @@
-const index = 1;
+let index = 0;
 
-const article = document.querySelector(`#slide${index}`);
-const radio = document.getElementsByClassName("radio-input");
 
-const slide = (id) => {
-    radio.find(radio.checked === true)
+const findRadios = document.getElementsByName('slider');
+const section = document.getElementById('slider-change');
+
+const radios = Array.from(findRadios);
+
+
+const findChecked = () => {
+    radios.forEach(radio => {
+        if(radio.checked){
+            index = radio.id;
+            let article = document.querySelector(`#slide${index}`);
+            article.classList.add('slide-active');
+        } else {
+            index = radio.id;
+            let article = document.querySelector(`#slide${index}`);
+            article.classList.remove('slide-active');
+        }
+    })
 }
+
+findChecked(); 
+const event = section.addEventListener('change', findChecked);
