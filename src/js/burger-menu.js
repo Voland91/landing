7465980cec2ba3797b8c9640de081b02d7ac9392
Mateusz Butkiewicz <butkiewicz.mat@gmail.com>
2021-08-26 16:43:30 +0200

    added sobe fexes to burger menu

--- src/js/burger-menu.js
+++ src/js/burger-menu.js
@@ -1,18 +1,20 @@
 let openIcon = document.getElementById(`burger`);
 let closeIcon = document.getElementById(`close`);
 let navigation = document.getElementById(`nav`);
+let navItems = document.getElementById(`nav-items`);
 
 const showBurgerMenu = () => {
     openIcon.classList.add('hidden');
     closeIcon.classList.remove('hidden');
-    navigation.classList.add('nav--burger')
+    navigation.classList.add('nav--burger');
 }
 
 const closeBurgerMenu= () => {
     openIcon.classList.remove('hidden');
     closeIcon.classList.add('hidden');
-    navigation.classList.remove('nav--burger')
+    navigation.classList.remove('nav--burger');
 }
 
 openIcon.addEventListener('click', showBurgerMenu);
-closeIcon.addEventListener('click', closeBurgerMenu)
+closeIcon.addEventListener('click', closeBurgerMenu);
+navItems.addEventListener('click', closeBurgerMenu);
